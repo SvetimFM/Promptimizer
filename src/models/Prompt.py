de32ca5e2d06d070739e00b8ed8ac73f1a2f1cdb
aleks A <aleks.allen@engineer.com>
2024-03-18 23:31:17 -0700

    pivoted to using OOP to make Promptimizer easier to edit - wrote out recursive optimization logic for the promptimizer core

--- src/models/Prompt.py
+++ src/models/Prompt.py
@@ -6,13 +6,17 @@ from pydantic import BaseModel, root_validator
 # TODO: Implement 'is_monte_carlo: bool' and its logic
 # TODO: Implement optimization for prompts for image generation and code output explicitly
 class Prompt(BaseModel):
-    gen: int                              # iteration of the prompt -> 0 is seed prompt
-    id_in_gen: int
-    id: Optional[str] = None              # id will be set by the root_validator
-    val: str                              # prompt itself
-    score: Optional[float] = None         # score of the prompt against test data
+    gen: int                                    # iteration of the prompt -> 0 is seed prompt
+    id_in_gen: int                              # id of the prompt in the generation
+    id: str = None                              # id will be set by the root_validator
+    val: str                                    # prompt itself
 
-    @root_validator(pre=True, allow_reuse=True)             # `pre=True` means this runs before field validation
+    optimization_vector: Optional[str] = None   # semantic critique of the prompt used to improve it
+    test_data: Optional[dict[any, any]] = None  # test data used to evaluate the prompt's quality
+    toa: Optional[str] = None                   # target of action used to optimize the prompt
+    score: Optional[float] = None               # score of the prompt against test data
+
+    @root_validator(pre=True, allow_reuse=True)  # `pre=True` means this runs before field validation
     def init_id(cls, values):
         gen = values.get('gen')
         id_in_gen = values.get('id_in_gen')
--- src/promptimizer/promptimizer.py
+++ src/promptimizer/promptimizer.py
@@ -2,86 +2,119 @@ from src.models.PromptimizerLLM import PromptimizerLLM
 from src.models.Prompt import Prompt
 
 
-# given an initial prompt (seed), and a target of action (error function to minimize), generate a universe of improved
-# prompts
-def _expand_prompt(prompt_candidate: str, target_of_action: str, expansion_factor: int, monte_carlo: bool = False) -> \
-        list[str]:
-    """
-    :param prompt_candidate: initial prompt to expand upon.
-    :param target_of_action: semantic error function to minimize.
-    :param expansion_factor: number of expansions to perform.
-    :param monte_carlo: whether 50% of expansions are semantically identical, but worded differently.
-    :return: list of expanded prompts.
-    """
+class Promptimizer:
+    def __init__(self,
+                 llm: PromptimizerLLM,
+                 seed_prompt: Prompt,
+                 winner_count: int = 1,
+                 target_of_action: str = None,
+                 example_data: dict[any, any] = None):
+        """
+        :param llm: LLM custom pydantic model to use for prompt generation.
+        :param seed_prompt: initial prompt custom pydantic model to expand upon.
+        :param expansion_factor: number of expansions to perform.
+        :param steps_factor: count of evolution steps to perform -> !!this will be deprecated in favor of semantic based completion (llm decides it cannot improve the prompt further)!!
+        :param winner_count: number of prompts to return.
+        :param target_of_action: semantic error function to minimize.
+        :param example_data: dictionary of example data (data is used for prompt scoring)
+        :return: named tuple of items, returning optimized prompt, score, list of improvements made from the original prompt
+        """
+        self.llm = llm
 
+        self.seed_prompt = seed_prompt
+        self.seed_prompt.score = self._score_prompt(seed_prompt, example_data)
+        self.toa = target_of_action
+        self.test_data = example_data
 
-# given a prompt, use LLM to score it against a target of action (error function to minimize)
-def _critique_prompt(prompt, target_of_action: str, test_data: dict[any, any] = None) -> str:
-    """
-    :param prompt: prompt to score.
-    :param target_of_action: semantic error function to minimize.
-    :param test_data: optional dictionary of example data (data is used for prompt scoring as test data)
-    :return: semantic error of the prompt against the target action, optionally tested against provided data.
-    """
-    pass
+        self.winner_count = winner_count
+        self.optimizedPrompts = None
 
+    # exposed method to train the prompt
+    def promptimize(self, expansion_factor: int = 10, steps_factor: int = 1, ):
+        return self._promptimize(self.seed_prompt, steps_factor, expansion_factor)
 
-def _error_correct_prompt(prompt: str, semantic_error: str, target_of_action: str) -> str:
-    """
-    :param prompt: prompt to correct.
-    :param semantic_error: semantic error of the prompt.
-    :param target_of_action: semantic error function to minimize.
-    :return: prompt with the semantic error corrected.
-    """
-    pass
+    @classmethod
+    def _promptimize(cls, prompt: Prompt, gen_limit: int, steps_limit: int):
+        prompt_generation_count = int(prompt.id.split("_")[0])
+        prompt_expansion_count = int(prompt.id.split("_")[1])
 
+        if prompt_generation_count == gen_limit:
+            return prompt
 
-# TODO: implement Successive Rejects
-# scores a universe of prompts against a target of action (error function to minimize)
-def _select_best_prompts(prompts: list[str], target_of_action: str, winner_count) -> list[str]:
-    """
-    :param prompts: list of prompts to score.
-    :param target_of_action: semantic error function to minimize.
-    :param winner_count: number of prompts to return.
-    :return: list of prompts with the best scores, up to 'winner_count'
-    """
-    pass
+        # STEP 1: ASSESS CURRENT PROMPT, CREATE GRADIENT
+        # score the prompt
+        # critique the prompt -> generate optimization gradient
 
+        # STEP 2: EXPAND AND IMPROVE PROMPT BASED ON GRADIENT STEPS TIMES
+        # expand the prompt based on the critique steps_limit times -> error correction step
+            # generation id of each prompt needs to be prompt_generation_count+=1
 
-# TODO: Implement training data and subsequent testing
-def run_optimizer(llm: PromptimizerLLM, prompt: Prompt, expansion_factor: int = 10, steps_factor: int = 1, winner_count: int = 1, target_of_action: str = None, example_data: dict[any, any] = None) -> list[str]:
-    """
-    :param llm: LLM to use for prompt generation.
-    :param prompt: initial prompt to expand upon.
-    :param expansion_factor: number of expansions to perform.
-    :param steps_factor: count of evolution steps to perform -> !!this will be deprecated in favor of semantic based completion (llm decides it cannot improve the prompt further)!!
-    :param winner_count: number of prompts to return.
-    :param target_of_action: semantic error function to minimize.
-    :param example_data: dictionary of example data (data is used for prompt scoring)
-    :return: named tuple of items, returning optimized prompt, score, list of improvements made from the original prompt
-    """
-    return [llm, prompt]
+        # STEP 3: SCORE AND SELECT BEST PROMPTS BASED ON SCORE
+        # _score_prompt the list of prompts
+            # if all prompts are less than current prompt in score, return current prompt (fold the arm)
+        # pick top <Count of arms> prompts
 
+        # STEP 4: RECURSE ON TOP PROMPTS
+        # for each top prompt, call promptimize
 
+    # given an initial prompt (seed), its target of action (error function to minimize), its semantic gradient,
+    # generate expansion_factor improved prompts
+    def _expand_prompt(self, prompt_candidate: Prompt, expansion_factor: int, monte_carlo: bool = False,) -> \
+            list[str]:
+        """
+        :param prompt_candidate: initial prompt to expand upon.
+        :param expansion_factor: number of expansions to perform.
+        :param monte_carlo: whether 50% of expansions are semantically identical, but worded differently.
+        :return: list of expanded prompts.
+        """
 
-#TODO:
-# -Summary:
-# consider implementation of a target_of_action optimizer, which, based on the user provided generalized action,
-# selects one of the following well-understood categories and optimizes for them (reducing the field of possibilities
-# over which the prompt has to be optimized)
-# -Categories:
-# needle_in_haystack
-# code_gen
-# code_gen_troubleshoot
-# text_gen_qa
-# text_gen_summarization
-# text_gen_translation
-# text_gen_writing
-# text_gen_chat
-# categorization
-# fact_checking
-# sentiment_analysis
+    # given a prompt, its score, training data, and semantic critique
+    # use LLM to score it against a target of action (error function to minimize)
+    def _critique_prompt(self, prompt: Prompt) -> str:
+        """
+        :param prompt: prompt to score.
+        :return: semantic error of the prompt against the target action, optionally tested against provided data.
+        """
+        pass
 
-# TODO: Implement optimization for prompts for image generation and code output explicitly
+    # TODO: implement Successive Rejects
+    # sorts and returns winner_count prompts from all prompts, based on their scores
+    def _select_best_prompts(self, prompts: list[Prompt], winner_count: int) -> list[str]:
+        """
+        :param prompts: list of prompts to score.
+        :param winner_count: number of prompts to return.
+        :return: list of prompts with the best scores, up to 'winner_count'
+        """
+        pass
 
-# TODO: Implement 'is_monte_carlo: bool' and its logic
+    # score a prompt against a target of action (error function to minimize) and optional test data
+    def _score_prompt(self, prompt: Prompt, data: dict[any, any] = None, semantic: bool = True) -> float:
+        """
+        :param prompt: prompt to score.
+        :param data: optional dictionary of example data (data is used for prompt scoring)
+        :param semantic: whether to score the prompt against the target of action (semantic error function)
+        :return: score of the prompt against the target of action (semantic error function)
+        """
+        pass
+
+    #TODO:
+    # -Summary:
+    # consider implementation of a target_of_action optimizer, which, based on the user provided generalized action,
+    # selects one of the following well-understood categories and optimizes for them (reducing the field of possibilities
+    # over which the prompt has to be optimized)
+    # -Categories:
+    # needle_in_haystack
+    # code_gen
+    # code_gen_troubleshoot
+    # text_gen_qa
+    # text_gen_summarization
+    # text_gen_translation
+    # text_gen_writing
+    # text_gen_chat
+    # categorization
+    # fact_checking
+    # sentiment_analysis
+
+    # TODO: Implement optimization for prompts for image generation and code output explicitly
+
+    # TODO: Implement 'is_monte_carlo: bool' and its logic
